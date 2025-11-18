8bc12671b8bb7f884717e45b6b17ab5456c1672a
Hasratdeep <hasratdeepkaur383@gmail.com>
2025-11-17 22:11:29 -0600

    Added user models file.

--- src/api/v1/models/taskModels.ts
+++ src/api/v1/models/taskModels.ts
@@ -1,5 +1,13 @@
 import mongoose from "mongoose";
 
+export interface Task {
+  name: string;
+  description?: string;
+  completed: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
 const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: String,
---
+++ src/api/v1/models/userModels.ts
@@ -0,0 +1,15 @@
+import mongoose, { Document } from "mongoose";
+
+export interface IUser extends Document {
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const UserSchema = new mongoose.Schema<IUser>({
+  email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+  password: { type: String, required: true }
+}, { timestamps: true });
+
+export default mongoose.model<IUser>("User", UserSchema);
