bb3dd78c054a680946d67d6fd72a13e9beb9153c
Hasratdeep <hasratdeepkaur383@gmail.com>
2025-11-24 18:52:10 -0600

    Added task and user controllers.

---
+++ src/api/v1/Controller/userController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import User, { IUser } from "../Model/userModel";
+
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const { username, email, password } = req.body;
+    const user: IUser = new User({ username, email, password });
+    await user.save();
+    res.status(201).json(user);
+  } catch (err: any) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+export const getUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err: any) {
+    res.status(500).json({ error: err.message });
+  }
+}; 
